4d03b55ba1d43af71815fa3b52247b1b3d5fc384
Jin KiHwan <eljsh95@naver.com>
2024-01-20 23:39:50 +0900

    [자습] 리스트 반복문으로 변경해봄

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,16 +1,16 @@
 <template>
-  <div>
-    <h5 @click="$router.go(-1)">{{ blogData[0].title }}</h5>
-    <p>{{ blogData[0].date }}</p>
+  <div v-for="(a, i) in 3" :key="i">
+    <h5 @click="$router.push(`/detail/${i}`)">{{ blogData[i].title }}</h5>
+    <p>{{ blogData[i].date }}</p>
   </div>
-  <div>
+  <!-- <div>
     <h5 @click="$router.push('/detail/1')">{{ blogData[1].title }}</h5>
     <p>{{ blogData[1].date }}</p>
   </div>
   <div>
     <h5 @click="$router.push('/detail/2')">{{ blogData[2].title }}</h5>
     <p>{{ blogData[2].date }}</p>
-  </div>
+  </div> -->
 </template>
 
 <script>
